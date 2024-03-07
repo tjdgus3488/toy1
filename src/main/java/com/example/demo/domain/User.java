e9921129ec071b3d7e5dbd24c493ee498bde4dd9
tjdgus3488 <tjdgus3488@naver.com>
2024-03-07 19:40:33 +0900

    toy1

--- src/main/java/com/example/demo/domain/User.java
+++ src/main/java/com/example/demo/domain/User.java
@@ -4,9 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+
 @Entity
 @Data
 public class User {
@@ -15,8 +17,8 @@ public class User {
     @GeneratedValue(strategy =  GenerationType.IDENTITY)
     private Long id;
 
-    @NotNull
     @Email
+    @NotNull
     private String email;
 
     private String password;
